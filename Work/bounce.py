9e58b91df5c9c699b6003a711c521b94cb20a7af
richard <richard@precode.co.za>
2023-12-04 11:23:52 +0000

    hello_world.md done

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # 100 meters
+return_height = 0.6 # 3 / 5 or 0.6 bounce back
+bounces = 10 # 10 bounces
+
+while bounces >= 1:
+    height *= 0.6
+    print(bounces, round(height, 4))
+    bounces -= 1
+
+pass
+
